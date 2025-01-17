00dea405ae2c5f74377ea6d9163e1d0982d7b86e
Vikash Kumar <vikashkrmuz0512@gmail.com>
2025-01-17 15:22:52 +0530

    Day4.3

---
+++ Day4/JavaScript/hello.js
@@ -0,0 +1,22 @@
+console.log("Hello, World!");
+var x = 5;
+var y = 10;
+var z = x + y;
+console.log(z);
+
+x="Hello";
+console.log(x);
+console.log(typeof x);
+console.log(typeof y);
+
+const v=["vikash","kumar",true,["pradeep","patel"]];
+console.log(v , typeof v);
+console.log(v[3][1]);
+
+const rohit={
+    name:"Rohit",
+    age:21,
+    isStudent:true,
+    friends:["Vikash","Pradeep"]
+}
+console.log(rohit);
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 This codebase it a practice and class-work of my winter pep classes on mern-stack.
+
+JavaScript is starting from Day 4.
